057f8bf31f1df4c543a85d2c7815e8f8202b402c
Christopher Fanning <chris@christopherfanning.com>
2022-02-23 17:12:52 -0600

    Add this proxy and add crossOrigin, probably useless.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './project/project.component';
 import { NewProjectComponent } from './new-project/new-project.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NewProjectComponent } from './new-project/new-project.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-project',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
+  // get the data.
+  projects: any;
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+
+  constructor(private http: HttpClient) { }
+
+
+ngOnInit(): void {
+  this.http
+    .get(
+      `http://localhost:9092/api/project`
+    )
+    .subscribe((response) => {
+      console.log(response);
+      this.projects = response;
+    });
+}
 
 }
