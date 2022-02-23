e29e923ebb38499079bd76344588ea69a2a2882e
Christopher Fanning <chris@christopherfanning.com>
2022-02-23 15:29:42 -0600

    Generate new-project component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './project/project.component';
+import { NewProjectComponent } from './new-project/new-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectComponent
+    ProjectComponent,
+    NewProjectComponent
   ],
   imports: [
     BrowserModule,
