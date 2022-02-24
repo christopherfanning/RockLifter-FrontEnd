198a353444b0e798cc01b7a28396ee734d2e28c4
Christopher Fanning <chris@christopherfanning.com>
2022-02-24 13:44:21 -0600

    Add some content and style to home and nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {NewProjectComponent} from "./new-project/new-project.component";
+import {HomeComponent} from "./home/home.component";
+import {ProjectComponent} from "./project/project.component";
 
 const routes: Routes = [
-
+  {
+    path: 'project',
+    component: ProjectComponent
+  },
   {
     path: 'project/new',
     component: NewProjectComponent
