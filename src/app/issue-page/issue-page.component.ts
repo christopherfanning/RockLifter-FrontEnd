5ecd4b09babcf796874f9e4f1ea4449eb055c71b
Christopher Fanning <chris@christopherfanning.com>
2022-03-01 15:45:36 -0600

    Build edit issue functionality.

--- src/app/issue-page/issue-page.component.ts
+++ src/app/issue-page/issue-page.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {FormBuilder, Validators} from "@angular/forms";
 import {environment} from "../../environments/environment";
+import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import { ElementRef } from "@angular/core";
+
+
 
 @Component({
   selector: 'app-issue-page',
@@ -12,6 +17,9 @@ export class IssuePageComponent implements OnInit {
 
   issue: any;
   issueId: any;
+  editIssueForm: any;
+
+  closeResult: any;
   baseUrl = environment.baseUrl;
 
   headers = new HttpHeaders()
@@ -19,8 +27,18 @@ export class IssuePageComponent implements OnInit {
     .set('Access-Control-Allow-Origin', '*');
 
   constructor(private route: ActivatedRoute,
-              private http: HttpClient
-              ) { }
+              private http: HttpClient,
+              private modalService: NgbModal,
+              private formBuilder: FormBuilder,
+              private ele: ElementRef
+              ) {
+
+    this.editIssueForm = this.formBuilder.group({
+      title:['', [Validators.required, Validators.minLength(1),Validators.maxLength(200)]],
+      description: ['', Validators.required],
+      status: ['']
+    });
+  }
 
   ngOnInit(): void {
     this.route.paramMap
@@ -39,4 +57,36 @@ export class IssuePageComponent implements OnInit {
       });
   }
 
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  issueSubmit(): void {
+    console.warn('Your post has been submitted', this.editIssueForm.value);
+    // this.postData.setItem(this.projectForm.value.id, JSON.stringify(this.projectForm.value))
+    this.http
+      .post(`${this.baseUrl}/api/issue/${this.issueId}`, JSON.stringify(this.editIssueForm.value),
+        {'headers':this.headers})
+      .subscribe((data) => {
+        console.log("I think this is the response:");
+        console.log(data);
+
+      });
+
+    this.editIssueForm.reset();
+  }
 }
