753e5500a14610bf0f61bee6c19d9f91d456089a
Christopher Fanning <chris@christopherfanning.com>
2022-03-01 15:46:31 -0600

    Generate Edit-issue component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NewIssueComponent } from './new-issue/new-issue.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { NgbNavModule} from "@ng-bootstrap/ng-bootstrap";
 import { IssuePageComponent } from './issue-page/issue-page.component';
+import { EditIssueComponent } from './edit-issue/edit-issue.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { IssuePageComponent } from './issue-page/issue-page.component';
     NavComponent,
     NewIssueComponent,
     ProjectListComponent,
-    IssuePageComponent
+    IssuePageComponent,
+    EditIssueComponent
   ],
     imports: [
         BrowserModule,
